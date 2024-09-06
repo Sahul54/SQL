32c5e351fb8e3c76dd09a27695f5b59add4b582f
sahul54 <ksahul9576@gmail.com>
2024-09-06 10:04:22 +0530

    first commit

---
+++ class01.sql
@@ -0,0 +1,13 @@
+create database student;
+
+use student;
+CREATE TABLE student (
+id INT primary KEY,
+name varchar(255)
+);
+
+Insert into student value (1, "ankit");
+
+select * from student;
+
+
